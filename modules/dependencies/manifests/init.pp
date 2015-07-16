e3586c50b0f053d63807cd2b5bc646ddee157bda
John Voloski <johnvoloski@cwi.com.br>
2015-07-16 12:18:25 -0300

    implements puppet modules

---
+++ modules/dependencies/manifests/init.pp
@@ -0,0 +1,196 @@
+class dependencies {
+  package {
+    'build-essential':
+      ensure => installed
+  }
+
+  package {
+    'gcc':
+      ensure => installed
+  }
+
+  package {
+    'make':
+      ensure => installed
+  }
+
+  package {
+    'ncurses-base':
+      ensure => installed
+  }
+
+  package {
+    'ncurses-dev':
+      ensure => installed
+  }
+
+  package {
+    'tcl':
+      ensure => installed
+  }
+
+  package {
+    'tcl-dev':
+      ensure => installed
+  }
+
+  package {
+    'libgdbm3':
+      ensure => installed
+  }
+
+  package {
+    'libgdbm-dev':
+      ensure => installed
+  }
+
+  package {
+    'libyaml-dev':
+      ensure => installed
+  }
+
+  package {
+    'libffi6':
+      ensure => installed
+  }
+
+  package {
+    'libffi-dev':
+      ensure => installed
+  }
+
+  package {
+    'readline-common':
+      ensure => installed
+  }
+
+  package {
+    'libreadline6':
+      ensure => installed
+  }
+
+  package {
+    'libreadline6-dev':
+      ensure => installed
+  }
+
+  package {
+    'zlib1g':
+      ensure => installed
+  }
+
+  package {
+    'zlib1g-dev':
+      ensure => installed
+  }
+
+  package {
+    'openssl':
+      ensure => installed
+  }
+
+  package {
+    'curl':
+      ensure => installed
+  }
+
+  package {
+    'libxml2':
+      ensure => installed
+  }
+
+  package {
+    'libxml2-dev':
+      ensure => installed
+  }
+
+  package {
+    'libxslt1.1':
+      ensure => installed
+  }
+
+  package {
+    'libxslt1-dev':
+      ensure => installed
+  }
+
+  package {
+    'fontconfig':
+      ensure => installed
+  }
+
+  package {
+    'libfontconfig1':
+      ensure => installed
+  }
+
+  package {
+    'libfontconfig1-dev':
+      ensure => installed
+  }
+
+  package {
+    'libfreetype6':
+      ensure => installed
+  }
+
+  package {
+    'libfreetype6-dev':
+      ensure => installed
+  }
+
+  package {
+    'unzip':
+      ensure => installed
+  }
+
+  package {
+    'tar':
+      ensure => installed
+  }
+
+  package {
+    'unrar':
+      ensure => installed
+  }
+
+  package {
+    'sudo':
+      ensure => installed
+  }
+
+  package {
+    'vim':
+      ensure => installed
+  }
+
+  package {
+    'imagemagick':
+      ensure => installed
+  }
+
+  package {
+    'xvfb':
+      ensure => installed
+  }
+
+  package {
+    'monit':
+      ensure => installed
+  }
+
+  package {
+    'htop':
+      ensure => installed
+  }
+
+  package {
+    'phantomjs':
+      ensure => installed
+  }
+
+  package {
+    'libpq-dev':
+      ensure => installed
+  }
+}
